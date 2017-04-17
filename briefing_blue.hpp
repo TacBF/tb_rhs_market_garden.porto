6cb9f6ec5bc9b12a0c93d5326a008b77cd4a496d
Ballista Milsim <ballista.milsim@gmail.com>
2017-04-17 16:42:24 +0200

    tb_rhs_market_garden.porto mission added

---
+++ briefing_blue.hpp
@@ -0,0 +1,18 @@
+// Provide side specific mission briefing here. Text can use structured text syntax. Use <br /> tag for line breaks, plus all other Arma tags.
+<titleFont>Gerneral Reminders:</titleFont><br />
+<*> <green>Squad leaders</green> - take the SL kit and deploy <RP> Rally Points<br />
+<*> Help out your team by deploying <FO> Forward Outposts<br />
+<br />
+
+<titleFont>Deployment Options:</titleFont><br />
+<boxes> Forward Base at Transmitter<br />
+<RP> Team Rally Point at Quarters<br />
+<br />
+
+<titleFont>Strategies:</titleFont><br />
+<*> Controlling Garage early on can give you an advantage<br />
+<*> Attacking Market before Storage is captured can slow down the enemy team<br />
+<br />
+
+<titleFont>Tips:</titleFont><br />
+<*> <yellow>You can only deploy two Forward Outposts, use them wisely</yellow>
